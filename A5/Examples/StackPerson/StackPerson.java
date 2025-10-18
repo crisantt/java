498dad622c995a433cf38d3b690046f3cb942100
crisantt <jcsanjose@proton.me>
2025-10-18 19:46:46 +0800

    Assignment 5 done

--- A5/Examples/StackPerson/StackPerson.java
+++ A5/Examples/StackPerson/StackPerson.java
@@ -1,92 +1,94 @@
 package StackPerson;
 
 public class StackPerson {
-    private Person[] persons;
-    private int top;
-    private int maxSize;
+	private Person[] persons;
+	private int top;
+	private int maxSize;
 
-    public StackPerson() {
-        this.maxSize = 5;
-        this.persons = new Person[maxSize];
-        this.top = -1;
-    }
+	public StackPerson() {
+		this.maxSize = 5;
+		this.persons = new Person[maxSize];
+		this.top = -1;
+	}
 
-    public StackPerson(int maxSize) {
-        this.maxSize = maxSize;
-        this.persons = new Person[maxSize];
-        this.top = -1;
-    }
+	public StackPerson(int maxSize) {
+		this.maxSize = maxSize;
+		this.persons = new Person[maxSize];
+		this.top = -1;
+	}
 
-    public boolean isFull() {
-        return top == maxSize - 1;
-    }
+	public boolean isFull() {
+		return top == maxSize - 1;
+	}
 
-    public boolean isEmpty() {
-        return top == -1;
-    }
+	public boolean isEmpty() {
+		return top == -1;
+	}
 
-    /*public void push(String firstName, String lastName, String email) {
-        if (!isFull()) {
-            Person person = new Person(firstName, lastName, email);
-            persons[++top] = person;
-        } else {
-            System.out.println("The stack is full!");
-        }
-    }
+	/*
+	 * public void push(String firstName, String lastName, String email) {
+	 * if (!isFull()) {
+	 * Person person = new Person(firstName, lastName, email);
+	 * persons[++top] = person;
+	 * } else {
+	 * System.out.println("The stack is full!");
+	 * }
+	 * }
+	 * 
+	 * public void pop() {
+	 * if (!isEmpty()) {
+	 * System.out.println(persons[top--].toString());
+	 * } else {
+	 * System.out.println("Stack is empty!");
+	 * }
+	 * }
+	 * 
+	 * public void peek() {
+	 * if (!isEmpty()) {
+	 * System.out.println(persons[top].toString());
+	 * } else {
+	 * System.out.println("Stack is empty!.");
+	 * }
+	 * }
+	 */
 
-    public void pop() {
-        if (!isEmpty()) {
-            System.out.println(persons[top--].toString());
-        } else {
-            System.out.println("Stack is empty!");
-        }
-    }
+	public void push(Person person) {
+		if (!isFull()) {
+			persons[++top] = person;
+		} else {
+			System.out.println("The stack is full!");
+		}
+	}
 
-    public void peek() {
-        if (!isEmpty()) {
-            System.out.println(persons[top].toString());
-        } else {
-            System.out.println("Stack is empty!.");
-        }
-    }*/
+	public Person pop() {
+		if (!isEmpty()) {
+			return persons[top--];
+		} else {
+			System.out.println("Stack is empty!");
+			return null;
+		}
+	}
 
-    public void push(Person person) {
-        if (!isFull()) {
-            persons[++top] = person;
-        } else {
-            System.out.println("The stack is full!");
-        }
-    }
+	public Person peek() {
+		if (!isEmpty()) {
+			return persons[top];
+		} else {
+			System.out.println("Stack is empty!.");
+			return null;
+		}
+	}
 
-    public Person pop() {
-        if (!isEmpty()) {
-            return persons[top--];
-        } else {
-            System.out.println("Stack is empty!");
-            return null;
-        }
-    }
+	public int size() {
+		return top + 1;
+	}
 
-    public Person peek() {
-        if (!isEmpty()) {
-            return persons[top];
-        } else {
-            System.out.println("Stack is empty!.");
-            return null;
-        }
-    }
-
-    public int size() {
-        return top + 1;
-    }
-
-    public void popAllItems() {
-        if (!isEmpty()) {
-            while (!isEmpty()) {
-                System.out.println(persons[top--].toString());
-            }
-        } else {
-            System.out.println("Stack is empty!");
-        }
-    }
+	public void popAllItems() {
+		if (!isEmpty()) {
+			while (!isEmpty()) {
+				System.out.println(persons[top--].toString());
+			}
+		} else {
+			System.out.println("Stack is empty!");
+		}
+	}
 }
--- A5/Main.java
+++ A5/Main.java
@@ -1,6 +1,21 @@
 public class Main {
 	public static void main(String[] args) {
+		Stack st = new Stack();
 		Book libro = new Book();
-		libro.setDetails("Data Analytics", "Author", "ADNU Press", "6th");
+
+		libro.setDetails("Data Analytics", "Jane Doe", "ADNU Press", "6th");
+		st.push(libro);
+		System.out.println(st.peek());
+
+		libro.setDetails("Differential Equations", "John Doe", "ADNU Press", "10th");
+		st.push(libro);
+		System.out.println(st.peek());
+
+		libro.setDetails("Basic of Programming", "Karla", "ADNU Press", "10th");
+		st.push(libro);
+		System.out.println(st.pop());
+
+		// Position of top
+		System.out.println("\nPosition of Top: " + st.size());
 	}
 }
